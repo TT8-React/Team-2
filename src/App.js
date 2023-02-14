3308711183f8ec092844a7d0583cd3137fb357f0
Anas-Alnabahin <anasnb2002nb@gmail.com>
2023-02-14 16:30:33 +0200

    clearing App component from imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,3 @@
-import Navbar from "./Components/Navbar/Navbar";
-import SideBar from "./Components/SideBar/SideBar";
-import Main from "./Components/Main/Main";
-import Report from "./Pages/Report";
-
 function App() {
   return (
     <div className="App">
