6c7a01fe151f8c6f6b84998f555b6024be200346
Jomanahani <jomanahani123456@gmail.com>
2023-02-14 12:47:36 +0200

    create Data tab

---
+++ src/Components/DataTab/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Tab, TabItem } from "./style";
+
+
+export default function DataTab() {
+  return (
+    <Tab>
+      <ul>
+        <TabItem>Manual data tab</TabItem>
+        <TabItem>Bulk upload tab</TabItem>
+      </ul>
+    </Tab>
+  );
+}
