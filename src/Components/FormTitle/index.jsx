f78e131d3fdd57b9026e7be3a2d95e67ec9feaef
hebamesluh1 <hebamesluh@gmail.com>
2023-02-14 12:36:57 +0200

    title form components

---
+++ src/Components/FormTitle/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Title} from './style';
+const FormTitle = ({title}) => {
+    return (
+        <Title>{title}</Title>
+    )
+}
+
+export default FormTitle
--- src/Pages/LogIn/index.jsx
+++ src/Pages/LogIn/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import FormItem from '../../Components/FormItem'
+import FormTitle from './../../Components/FormTitle/index';
 
 const LogIn = () => {
     return (
         <div>
+            <FormTitle title="LOGIN"/>
             <FormItem label="test" name="test2"/>
         </div>
     )
