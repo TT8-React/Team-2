040c4416042ff961291b4e642e9c9c9eb91cffaa
hebamesluh1 <hebamesluh@gmail.com>
2023-02-15 00:28:16 +0200

    add wrapper form component

---
+++ src/Components/WrapperForm/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {FormWrapper} from './style'
+const WrapperForm = ({children}) => {
+    return (
+        <FormWrapper>{children}</FormWrapper>
+    )
+}
+
+export default WrapperForm
