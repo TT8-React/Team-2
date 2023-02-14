953851c4eb80ab520c5143d07e59d3d7f583c37c
Abood10s <saafeenabood@gmail.com>
2023-02-14 22:31:52 +0200

    specifying the Button color

--- src/Components/Button/style.js
+++ src/Components/Button/style.js
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 
 export const Btn=styled.button`
-  background: #000000;
-  border-radius: 5px;
-  color: #fff;
-  font-weight: 700;
-  font-size: 16px;
-  line-height: 19px;
-  padding: 10px 16px;
-`;
+    background: #000000;
+    border-radius: 5px;
+    color:#fff;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 19px;
+    padding: 10px 16px;
+`
