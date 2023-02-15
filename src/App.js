bb75343ea12b33d06068cb5566459f6fdbeb8dbd
Abood10s <saafeenabood@gmail.com>
2023-02-15 14:11:13 +0200

    Emptying App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
-import Layout from "./Components/Layout";
-import ReportGrid from "./Components/ReportGrid";
-import TableTwoCols from "./Components/TableTwoCols";
-import { TwoColsTableData } from "./MockData/Data";
 function App() {
-  return (
-    <div className="App">
-      <Layout>
-        <ReportGrid>
-          <TableTwoCols data={TwoColsTableData} />
-        </ReportGrid>
-      </Layout>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
