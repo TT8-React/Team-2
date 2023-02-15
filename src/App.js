61b3eaaf509e22817b82d93c534aebe3ff2b48b8
Abood10s <saafeenabood@gmail.com>
2023-02-15 14:08:53 +0200

    adding Accordions and Two columns Tables with a single component holding them dynamically

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import Layout from "./Components/Layout";
+import ReportGrid from "./Components/ReportGrid";
+import TableTwoCols from "./Components/TableTwoCols";
+import { TwoColsTableData } from "./MockData/Data";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Layout>
+        <ReportGrid>
+          <TableTwoCols data={TwoColsTableData} />
+        </ReportGrid>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/ReportGrid/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  justify-content: center;
+  width: 100%;
+  margin-left: 10%;
+  gap: 1.5rem;
+`;
