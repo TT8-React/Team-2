b005c4142fe309e18a1d10c476c7647d4b371bbe
hebamesluh1 <hebamesluh@gmail.com>
2023-02-15 04:39:38 +0200

    update style

--- src/Components/FormItem/index.jsx
+++ src/Components/FormItem/index.jsx
@@ -3,10 +3,12 @@ import {WrapperItem,Input} from './style';
 
 const FormItem = ({name,label,type}) => {
     return (
+        <>
         <WrapperItem>
             <label htmlFor={name}>{label}</label>
             <Input type={type} placeholder={`Enter ${label}`} id={name} name={name}/>
         </WrapperItem>
+        </>
     )
 }
 
--- src/Components/FormItem/style.js
+++ src/Components/FormItem/style.js
@@ -2,6 +2,10 @@ import styled from "styled-components";
 
 export const WrapperItem = styled.div`
     color:#52525C;
+    width: 360px;
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 25px;
     label{
         font-family: 'Inter';
         font-style: normal;
