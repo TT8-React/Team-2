156db86cc52744e2f9d16747fbcdaca1c89596c1
Abood10s <saafeenabood@gmail.com>
2023-02-15 14:47:30 +0200

    adding two columns form

---
+++ src/Components/TowColForm/style.js
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  row-gap: 0.2em;
+  column-gap: 0.5em;
+  place-items: center;
+  width: 80%;
+`;
+export const Form = styled.form`
+  text-align: center;
+`;
