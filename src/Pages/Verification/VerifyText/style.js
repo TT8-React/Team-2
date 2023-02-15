11f1136f6fdd9b2ef740caf35f7b5320c0dc5770
hebamesluh1 <hebamesluh@gmail.com>
2023-02-15 11:14:12 +0200

    finished ui of verification page

---
+++ src/Pages/Verification/VerifyText/style.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+export const Text = styled.p`
+    display: inline;
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 39px;
+    color: #000000;
+    span{
+        color:#f00;
+    }
+`
+export const Wrapper = styled.div`
+    padding:2rem;
+`
+export const Circle = styled.div`
+    display:inline-block;
+    margin-right:16px;
+    width: 16px;
+    margin-top: 6%;
+    height: 16px;
+    background:#7E7E7E;
+    border-radius: 50%;
+`
