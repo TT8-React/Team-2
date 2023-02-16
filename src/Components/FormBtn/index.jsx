ba26a4a3edf9db941c6ac739df4365072d09ba5a
Abood10s <saafeenabood@gmail.com>
2023-02-16 08:45:41 +0200

    adding formbtn edit

--- src/Components/FormBtn/index.jsx
+++ src/Components/FormBtn/index.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import { Btn } from './style';
 const FormBtn = ({ name }) => {
     return ( <
-        Btn type = 'submit' > { name } < /Btn>
-    )
+        >
+        <
+        Btn type = 'submit' > { name } < /Btn> <
+        />
+    );
 }
 
 export default FormBtn
