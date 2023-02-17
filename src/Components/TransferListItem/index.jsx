76ebf5f54471872e981285032370877a7744f57d
Anas-Alnabahin <anasnb2002nb@gmail.com>
2023-02-18 01:12:16 +0200

    Finishing adjustments for the transferlist

--- src/Components/TransferListItem/index.jsx
+++ src/Components/TransferListItem/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { TListItem } from './style'
+import arrow from '../../Assets/arrow.png'
 
 export default function TransferListItem(props) {
   return (
-    <TListItem><p>{props.children} </p><span onClick={props.handleTransfer}>&gt;</span></TListItem>
+    <TListItem><p>{props.children} </p><span onClick={props.handleTransfer}><img src={arrow} alt='arrow'/></span></TListItem>
   )
 }
