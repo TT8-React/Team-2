5a97328bdf87ad25fa0f4a87803501a9c4a6234d
Abood10s <saafeenabood@gmail.com>
2023-02-17 10:51:03 +0200

    adjusting TextForm margins

--- src/Components/TextForm/style.js
+++ src/Components/TextForm/style.js
@@ -7,7 +7,7 @@ export const Text = styled.p`
   font-size: 16px;
   line-height: 19px;
   color: #000000;
-  margin: 5px 0 10px 0;
+  margin: 2rem 0 10px 0;
   text-align: center;
   span {
     color: #ff0000;
