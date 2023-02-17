310c95361172fd19edd94d0c13c58b3c7974a853
Anas-Alnabahin <anasnb2002nb@gmail.com>
2023-02-18 00:47:37 +0200

    Finishing Company details page

---
+++ src/Pages/CompanyDetails/style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+width: 1142px;
+height: 550px;
+background: #F2F2F2;
+border: 1px solid #989898;
+border-radius: 10px;
+margin-top: 24px;
+padding: 38px;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+`
