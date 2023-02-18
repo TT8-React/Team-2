7d7a5ede6832b0215b3959f44dd488e46044025c
hebamesluh1 <hebamesluh@gmail.com>
2023-02-18 09:12:52 +0200

    add useAuthContext and useAuth hook

---
+++ src/Context/AuthContext.js
@@ -0,0 +1,17 @@
+import useAuth from "../hook/useAuth";
+import { createContext, useContext } from 'react';
+
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+    const auth = useAuth();
+    return (
+        <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
+
+export const useAuthContext = () => {
+    return useContext(AuthContext);
+};
