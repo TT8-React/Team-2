beeef494f4ee4d2cd67d4552ebed67fc4f7b64aa
Abood10s <saafeenabood@gmail.com>
2023-02-19 21:29:28 +0200

    fixing spinner height & width

--- src/Pages/CompanyDetails/index.jsx
+++ src/Pages/CompanyDetails/index.jsx
@@ -4,34 +4,28 @@ import { CompanyDetailsData } from "../../MockData/Data";
 import { Container } from "./style";
 
 export default function CompanyDetails() {
-    return ( <
-        >
-        <
-        div style = {
-            {
-                marginTop: "45px",
-                marginLeft: "1rem",
-                width: "95%",
-                display: "flex",
-                justifyContent: "end",
-            }
-        } >
-        <
-        Button name = { "Edit company details" }
-        />{" "} <
-        /div> <
-        Container > {
-            CompanyDetailsData.map((item) => {
-                return ( <
-                    div key = { item.id } >
-                    <
-                    p > { item.name } < /p> <h4> {item.text} </h
-                    4 > { " " } <
-                    /div>
-                );
-            })
-        } <
-        /Container> <
-        />
-    );
+  return (
+    <>
+      <div
+        style={{
+          marginTop: "45px",
+          marginLeft: "1rem",
+          width: "95%",
+          display: "flex",
+          justifyContent: "end",
+        }}
+      >
+        <Button name={"Edit company details"} />{" "}
+      </div>
+      <Container>
+        {CompanyDetailsData.map((item) => {
+          return (
+            <div key={item.id}>
+              <p> {item.name} </p> <h4> {item.text} </h4>{" "}
+            </div>
+          );
+        })}
+      </Container>
+    </>
+  );
 }
