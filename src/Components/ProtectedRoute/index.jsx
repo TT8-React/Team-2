e2ad55f0e78960adf4ea8038d27327b6ec51e03a
Abood10s <saafeenabood@gmail.com>
2023-02-20 12:58:17 +0200

    project prototype

--- src/Components/ProtectedRoute/index.jsx
+++ src/Components/ProtectedRoute/index.jsx
@@ -2,27 +2,22 @@ import { Navigate, Outlet } from "react-router-dom";
 import { PATHS } from "../../Routes";
 import { useAuthContext } from "../../Context/AuthContext";
 import { useLocation } from "react-router-dom";
-import Navbar from "./../Navbar/Navbar";
-import SideBar from "./../SideBar/SideBar";
-import Main from "./../Main/Main";
-
+import Layout from "../Layout/index";
 const ProtectedRoute = () => {
   const { pathname } = useLocation();
 
   const getPageName = (pathname) => {
     let str = pathname;
-    return str.split("/").length<3?str.split("/")[1]:str.split("/")[2];
+    return str.split("/").length < 3 ? str.split("/")[1] : str.split("/")[2];
   };
   const { authorized } = useAuthContext();
   return (
     <div>
       {authorized ? (
         <>
-          <Navbar path={getPageName(pathname)} />
-          <SideBar />
-          <Main>
+          <Layout path={getPageName(pathname)}>
             <Outlet />
-          </Main>
+          </Layout>
         </>
       ) : (
         <Navigate to={PATHS.LOGIN} />
